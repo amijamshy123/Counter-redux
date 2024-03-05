8ce8ac98cd9c0638703fbc9b3462a5143cd5a1b8
Amy <amijamsheer@gmail.com>
2024-03-05 16:52:16 +0300

    compleated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './Components/Counter'
+
+
+function App() {
+   return (
+    <>
+
+    <h1 className='text-centre mt-5'>counter project</h1>
+      <div style={{height:'70%'}} className='d-flex justify-content-center 
+      align-items-center w-100'></div>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
